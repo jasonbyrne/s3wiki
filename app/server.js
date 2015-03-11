0964331be9856db3f00fde4581ed1cf6b5d9e7ad
Jason Byrne <jason.byrne@flocasts.com>
2015-03-10 23:33:29 -0400

    add restify

---
+++ app/server.js
@@ -0,0 +1,51 @@
+/*
+ *   BRAIN GAME
+ *
+ */
+
+
+var restify = require('restify'),
+    fs = require('fs');
+
+
+var sendFile = function(req, res, next) {
+    var filePath = req.params.name;
+    if (req.params.level) {
+        filePath = req.params.level + '/' + filePath;
+    }
+    fs.readFile('./data/' + filePath + '.json', 'utf8', function(err, data) {
+        if (err) {
+            res.send("File Not Found");
+        }
+        else {
+            res.send(JSON.parse(data));
+        }
+    });
+};
+
+
+function respond(req, res, next) {
+    res.send({
+        'hello': req.params.name
+    });
+    next();
+}
+
+// initialize server
+var server = restify.createServer({
+    "name": "oldflorida.info"
+});
+server.pre(restify.pre.sanitizePath());
+
+// Test
+server.get('/hello/:name', respond);
+
+// Fallback for everything is local static file
+server.get(/\.*/, restify.serveStatic({
+    directory: './public',
+    default: 'index.html'
+}));
+
+server.listen((process.env.PORT || 80), function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
