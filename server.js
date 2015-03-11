c52c9a073e2c6453a102a08e3952725b06bbe42c
Jason Byrne <jason.byrne@flocasts.com>
2015-03-11 02:11:26 -0400

    trying to get static to work

--- server.js
+++ server.js
@@ -7,8 +7,6 @@
 var webRoot = './public',
     restify = require('restify'),
     fs = require('fs'),
-    serveStatic = require('node-static'),
-    staticFile = new(static.Server)(webRoot),
     config = {};
 
 
@@ -34,6 +32,18 @@ var sendFile = function(req, res, next) {
     });
 };
 
+var sendStatic = function(req, res, next) {
+    var filePath = req.params.name;
+    fs.readFile(webRoot + filePath, 'utf8', function(err, data) {
+        if (err) {
+            res.send("File Not Found");
+        }
+        else {
+            res.send(data);
+        }
+    });
+};
+
 
 function respond(req, res, next) {
     res.send({
@@ -52,19 +62,7 @@ server.pre(restify.pre.sanitizePath());
 server.get('/hello/:name', respond);
 
 // Fallback for everything is local static file
-server.get(/.*/, function (req, res) {
-    staticFile.serve(req, res, function(err, result) {
-        if (err) {
-            console.error('Error serving %s - %s', req.url, err.message);
-            res.writeHead(err.status, err.headers);
-            res.write(err.status);
-            res.end();
-        }
-        else {
-            console.log('%s - %s', req.url, res.message);
-        }
-    });
-});
+server.get(/.*/, sendStatic);
 
 server.listen((process.env.PORT || 80), function() {
     console.log('%s listening at %s', server.name, server.url);
